30ee80c6e63ae2a4cce2d706f303b93c12a7131d
YSDimov <ysdimov@live.com>
2023-08-09 15:23:56 +0300

    Update output.tf

--- caf_solution/add-ons/caf_eslz/output.tf
+++ caf_solution/add-ons/caf_eslz/output.tf
@@ -2,3 +2,7 @@ output "objects" {
   value     = module.enterprise_scale
   sensitive = true
 }
+
+output "archetype_policy_assignments_list" {
+  value = local.archetype_policy_assignments_list 
+}
